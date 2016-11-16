d03dc4814eeeac99de093ca90c17b5e6d419a93a
Zhe Wang <akina.dwnhl@gmail.com>
2016-11-16 15:22:39 -0800

    heroku push

--- frontend/components/readings/inspirations.jsx
+++ frontend/components/readings/inspirations.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 class Inspirations extends React.Component {
   render(){
     return(
-      <div>
-        Inspirations
+      <div className='quotes'>
+        "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it." - Brian W. Kernighan.
       </div>
     );
   }
