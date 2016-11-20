a595229960e69cbc522f3e07cf560515e7c706e4
Zhe Wang <akina.dwnhl@gmail.com>
2016-11-20 00:24:07 -0800

    adjusted color schemes

--- frontend/components/readings/inspirations.jsx
+++ frontend/components/readings/inspirations.jsx
@@ -4,8 +4,11 @@ class Inspirations extends React.Component {
   render(){
     return(
       <div className='quotes'>
-        <div>"Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it."</div>
-        <div>- Brian W. Kernighan.</div>
+        <img src='https://res.cloudinary.com/cloudlicious/image/upload/v1479628603/cloudcoder_p70n9u.png' />
+        <div className='text'>
+          <div>"Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it."</div>
+          <div>- Brian W. Kernighan.</div>
+        </div>
       </div>
     );
   }
