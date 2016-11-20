527da54197a45fc637082b40d70c388ee4504160
Zhe Wang <akina.dwnhl@gmail.com>
2016-11-19 20:56:52 -0800

    added pic to am

--- frontend/components/about_me.jsx
+++ frontend/components/about_me.jsx
@@ -4,7 +4,24 @@ class AboutMe extends React.Component {
   render(){
     return(
       <div className='about-me display-none'>
-        about me
+        <div className='about-me-container'>
+          <div>about me</div>
+          <div className='am-header'>
+            <div>
+              <h1>Zhe</h1>
+              <h1>Wang</h1>
+            </div>
+            <div>
+              <img src='https://res.cloudinary.com/cloudlicious/image/upload/v1479617027/profile_ydfq0z.jpg'/>
+            </div>
+          </div>
+          <div className='am-body'>
+            Body
+          </div>
+          <div className='am-misc'>
+            Misc
+          </div>
+        </div>
       </div>
     );
   }
