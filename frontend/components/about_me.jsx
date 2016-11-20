619d668e55284e4954aed27c6f1e38d26640e5ba
Zhe Wang <akina.dwnhl@gmail.com>
2016-11-19 21:54:14 -0800

    adjusted all font size, resize elements accordingly

--- frontend/components/about_me.jsx
+++ frontend/components/about_me.jsx
@@ -7,11 +7,11 @@ class AboutMe extends React.Component {
         <div className='about-me-container'>
           <div>about me</div>
           <div className='am-header'>
-            <div>
+            <div className='am-header-name'>
               <h1>Zhe</h1>
               <h1>Wang</h1>
             </div>
-            <div>
+            <div className='am-img-container'>
               <img src='https://res.cloudinary.com/cloudlicious/image/upload/v1479617027/profile_ydfq0z.jpg'/>
             </div>
           </div>
