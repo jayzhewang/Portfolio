8d07ba5cfc75eb67578ab07108f7d52b883cc9f1
Zhe Wang <akina.dwnhl@gmail.com>
2016-11-21 13:37:10 -0800

    resize

--- frontend/components/readings/inspirations.jsx
+++ frontend/components/readings/inspirations.jsx
@@ -6,8 +6,10 @@ class Inspirations extends React.Component {
       <div className='quotes'>
         <img src='https://res.cloudinary.com/cloudlicious/image/upload/v1479628603/cloudcoder_p70n9u.png' />
         <div className='text'>
-          <div>"Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it."</div>
-          <div>- Brian W. Kernighan.</div>
+          <div>
+            <div>"Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it."</div>
+            <div>- Brian W. Kernighan.</div>
+          </div>
         </div>
       </div>
     );
