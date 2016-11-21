3135161eb5233aaf8a1fdee6b24318d5600161e9
Zhe Wang <akina.dwnhl@gmail.com>
2016-11-21 01:41:17 -0800

    added assets icons

--- frontend/components/readings/assets.jsx
+++ frontend/components/readings/assets.jsx
@@ -3,8 +3,30 @@ import React from 'react';
 class Assets extends React.Component {
   render(){
     return(
-      <div>
-        Assets
+      <div className='assets'>
+        <div>
+          <div id='icon'><img src='https://res.cloudinary.com/cloudlicious/image/upload/c_scale,w_150/v1479719540/ruby_jtjei1.png'/></div>
+          <div id='icon'><img src='https://res.cloudinary.com/cloudlicious/image/upload/c_scale,w_150/v1479720122/js_jewchp.png'/></div>
+          <div id='icon'><img src='https://res.cloudinary.com/cloudlicious/image/upload/c_scale,w_150/v1479719540/Ruby_on_Rails.svg_xmspje.png'/></div>
+          <div id='icon'><img src='https://res.cloudinary.com/cloudlicious/image/upload/c_scale,w_150/v1479720122/react_zo24q8.png'/></div>
+          <div id='icon'><img src='https://res.cloudinary.com/cloudlicious/image/upload/c_scale,w_150/v1479719341/postgresql_mguxhq.png'/></div>
+        </div>
+        <div>
+          <div id='icon'><img src='https://res.cloudinary.com/cloudlicious/image/upload/v1479719341/nodejs_hx05m8.png'/></div>
+          <div id='icon'><img src='https://res.cloudinary.com/cloudlicious/image/upload/c_scale,w_150/v1479719341/logo_k9kox7.png'/></div>
+          <div id='icon'><img src='https://res.cloudinary.com/cloudlicious/image/upload/c_scale,w_150/v1479719340/flux-logo_zzwthw.png'/></div>
+          <div id='icon'><img src='https://res.cloudinary.com/cloudlicious/image/upload/c_scale,w_150/v1479719341/jquery_ip3upm.png'/></div>
+          <div id='icon'><img src='https://res.cloudinary.com/cloudlicious/image/upload/c_scale,w_150/v1479719342/rspec_plf8bh.png'/></div>
+          <div id='icon'><img src='https://res.cloudinary.com/cloudlicious/image/upload/c_scale,w_150/v1479719342/socket_uxi3ir.png'/></div>
+          <div id='icon'><img src='https://res.cloudinary.com/cloudlicious/image/upload/c_scale,w_150/v1479719341/java_gewgap.png'/></div>
+        </div>
+        <div>
+          <div id='icon'><img src='https://res.cloudinary.com/cloudlicious/image/upload/c_scale,w_150/v1479720524/git_ymzzbh.png'/></div>
+          <div id='icon'><img src='https://res.cloudinary.com/cloudlicious/image/upload/c_scale,w_150/v1479719340/html5_a5rlpv.png'/></div>
+          <div id='icon'><img src='https://res.cloudinary.com/cloudlicious/image/upload/c_scale,w_150/v1479719340/css3_sjjf6x.png'/></div>
+          <div id='icon'><img src='https://res.cloudinary.com/cloudlicious/image/upload/c_scale,w_150/v1479719340/bootstrap_q43pue.png'/></div>
+          <div id='icon'><img src='https://res.cloudinary.com/cloudlicious/image/upload/v1479719340/ajax_bwnllo.jpg'/></div>
+        </div>
       </div>
     );
   }
