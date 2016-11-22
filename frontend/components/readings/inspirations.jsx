6fce327370984b8df45fa7d4ad06e2e481887207
Zhe Wang <akina.dwnhl@gmail.com>
2016-11-22 09:55:41 -0800

    added quotes

--- frontend/components/readings/inspirations.jsx
+++ frontend/components/readings/inspirations.jsx
@@ -7,8 +7,11 @@ class Inspirations extends React.Component {
         <img src='https://res.cloudinary.com/cloudlicious/image/upload/v1479628603/cloudcoder_p70n9u.png' />
         <div className='text'>
           <div>
-            <div>"Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it."</div>
-            <div>- Brian W. Kernighan.</div>
+            <div className='quote-line'>
+              <div><span id='quotation-mark'>“</span></div>
+              <div>Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.<span id='quotation-mark'>​‌”</span></div>
+            </div>
+            <div>Brian W. Kernighan</div>
           </div>
         </div>
       </div>
