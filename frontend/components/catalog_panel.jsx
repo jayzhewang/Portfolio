0b4f63aa17c9099e152947d7816baab0720c4a7a
Zhe Wang <akina.dwnhl@gmail.com>
2016-11-23 10:14:57 -0800

    added smooth scrolling

--- frontend/components/catalog_panel.jsx
+++ frontend/components/catalog_panel.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+require('smoothscroll-polyfill').polyfill();
 
 class Catalog extends React.Component {
+  constructor(props){
+    super(props);
+    this.smoothScroll = this.smoothScroll.bind(this);
+  }
   componentDidMount(){
     let catalog = $('.catalog');
     catalog.addClass('catalog-panel-slider catalog-shift-left');
@@ -9,14 +14,18 @@ class Catalog extends React.Component {
     }, 800);
   }
 
+  smoothScroll(element){
+    document.querySelector(`#${element}`).scrollIntoView({ behavior: 'smooth' });
+  }
+
   render(){
     return(
       <div className='catalog'>
         <ul>
-          <li type='none'><a href="#inspirations">inspirations</a></li>
-          <li type='none'><a href="#projects">projects</a></li>
-          <li type='none'><a href="#assets">assets</a></li>
-          <li type='none'><a href="#contact">contact</a></li>
+          <li type='none' onClick={()=>this.smoothScroll('inspirations')}>inspirations</li>
+          <li type='none' onClick={()=>this.smoothScroll('projects')}>projects</li>
+          <li type='none' onClick={()=>this.smoothScroll('assets')}>assets</li>
+          <li type='none' onClick={()=>this.smoothScroll('contact')}>contact</li>
         </ul>
       </div>
     );
