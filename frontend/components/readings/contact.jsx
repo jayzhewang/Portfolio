4503d8f5c105ac0a680bcbfa9a3cf59de88be237
Zhe Wang <akina.dwnhl@gmail.com>
2016-11-24 23:32:34 -0800

    resized images css, added contact panel iputs

--- frontend/components/readings/contact.jsx
+++ frontend/components/readings/contact.jsx
@@ -1,10 +1,46 @@
 import React from 'react';
 
 class Contact extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      message: ''
+    };
+  }
+
+  update(field){
+    return e => { this.setState({[field]: e.currentTarget.value }); };
+  }
+
   render(){
     return(
       <div className='contact'>
-        Contact
+        <div className='contact-inner'>
+          <div>
+            <h1>Let's Talk</h1>
+            <p>Projects. Partnerships. Coffee.</p>
+          </div>
+          <div className='contact-input'>
+            <label for='contact-name'>Name</label>
+            <input value={this.state.name}
+              id='contact-name'
+              onChange={this.update('name')}/>
+          </div>
+          <div className='contact-input'>
+            <label for='contact-email'>Email</label>
+            <input value={this.state.email}
+              id='contact-email'
+              onChange={this.update('email')}/>
+          </div>
+          <div className='contact-input'>
+            <label for='contact-message'>Message</label>
+            <input value={this.state.message}
+              id='contact-message'
+              onChange={this.update('message')}/>
+          </div>
+        </div>
       </div>
     );
   }
